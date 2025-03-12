eccb0e66f177b97ba78e8f95cbbfd5ecea540a6d
laranegrii <laralayzanegri@gmail.com>
2025-03-12 18:16:27 -0300

    implementação de rota

--- app/Http/Controllers/Principal.php
+++ app/Http/Controllers/Principal.php
@@ -7,6 +7,9 @@ use Illuminate\Http\Request;
 class Principal extends Controller
 {
     function principal(){
-        echo 'Página Principal';
+        return View('principal');
+    }
+    function teste(){
+        return View('testado com sucesso!');
     }
 }
